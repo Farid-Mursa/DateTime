2afad7090b420bb09b3e90da4dec75a6324672f1
Farid-Mursa <117182057+Farid-Mursa@users.noreply.github.com>
2023-08-25 16:40:08 +0400

    Add files via upload

---
+++ AddDateTime/AddDateTime/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualBasic;
+
+namespace AddDateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter datetime in this type dd/MM/yyyy");
+            string date = Console.ReadLine();
+            DateTime addedTime = DateTime.ParseExact(date,"dd/MM/yyyy", null).AddDays(30).AddHours(6).AddMinutes(17);
+            Console.WriteLine(addedTime.ToString());
+            
+
+        }
+    }
+}
---
+++ ParseExact/ParseExact/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ParseExact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter date in this type dd | MM | yyyy");
+            string date = Console.ReadLine();
+            try
+            {
+                DateTime dateTime = DateTime.ParseExact(date, "dd | MM | yyyy", null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
